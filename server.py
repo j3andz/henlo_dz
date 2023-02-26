2db51c5a8d4e93f6289bf8fd7d49a13226b9a9a3
j3andz <123285788+j3andz@users.noreply.github.com>
2023-02-26 17:46:01 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import aiohttp
 from aiohttp import web
 
@@ -7,8 +5,10 @@ async def index(request):
     with open('index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
 
+
 app = web.Application()
 app.add_routes([web.get('/', index)])
 
 if __name__ == '__main__':
-    web.run_app(app)
+    
+    web.run_app(app, host='0.0.0.0', port=8080)
