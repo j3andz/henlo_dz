31d869300cc3c16171386af0a09d01d04bd352a8
j3andz <123285788+j3andz@users.noreply.github.com>
2023-02-26 17:39:57 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python3
 
-import socket
-import http.server
-import socketserver
+import aiohttp
+from aiohttp import web
 
-#PORT = int(input('Puerto: '))
-PORT = 1485 
+async def index(request):
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("gmail.com", 80))
-localIP = s.getsockname()[0]
-s.close()
-print('''
-  _   _   _____   _   _   _                       _       
- | | | | | ____| | \ | | | |   ___             __| |  ____
- | |_| | |  _|   |  \| | | |  / _ \           / _` | |_  /
- |  _  | | |___  | |\  | | | | (_) |         | (_| |  / / 
- |_| |_| |_____| |_| \_| |_|  \___/   _____   \__,_| /___|
-                                     |_____|              
-''')
-print("Servidor Iniciado en " + localIP + ":" + str(PORT))
+app = web.Application()
+app.add_routes([web.get('/', index)])
 
-Handler = http.server.SimpleHTTPRequestHandler
-server = socketserver.TCPServer(('', PORT), Handler)
-server.serve_forever()
+if __name__ == '__main__':
+    web.run_app(app)
